9954df61f1d62a4ef6a52c762665581630d119fc
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-06-30 21:51:53 +0700

    Add

--- myapp/views.py
+++ myapp/views.py
@@ -6,3 +6,9 @@ def index(request):
         'current_date': datetime.now().strftime('%d/%m/%Y %H:%M:%S')
     }
     return render(request, 'myapp/index.html', context)
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
